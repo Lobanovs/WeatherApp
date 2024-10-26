03ddbe0c649d166647dfac1d1a240001242e6928
Lobanovs <l0banov.simeon@yandex.ru>
2024-10-26 19:29:19 +0700

    Рабочая валидация ( закончен основной функционал )

--- src/main/java/by/lobanovs/weatherapp/util/Measurement/MeasurementNotCreatedException.java
+++ src/main/java/by/lobanovs/weatherapp/util/Measurement/MeasurementNotCreatedException.java
@@ -1,5 +1,7 @@
 package by.lobanovs.weatherapp.util.Measurement;
 
 public class MeasurementNotCreatedException extends RuntimeException {
-    public MeasurementNotCreatedException(String message) {}
+    public MeasurementNotCreatedException(String message) {
+        super(message);
+    }
 }
